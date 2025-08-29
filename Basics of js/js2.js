de710541d32534a757d6ec68a187ccc80c09182b
Chandana HR <77909453+ChandanaHR@users.noreply.github.com>
2025-08-29 15:53:09 +0530

    Add files via upload

---
+++ Basics of js/js2.js
@@ -0,0 +1,106 @@
+// Type conversion:
+//     Converting from one datatype to another datatype
+//     Implicit conversion, Explicit conversion
+//Implicit conversion
+let no1 = '5'+2
+console.log(no1)
+console.log(typeof no1)
+
+let no2 = 5+true
+console.log(no2)
+console.log(typeof no2)
+
+let result3 = '5' * 2;
+console.log(result3);
+console.log(typeof result3)
+
+console.log(5 == '5');
+
+// //Explicit conversion
+let num = Number("123");
+console.log(typeof num)
+console.log(num);
+
+let str = String(123);
+console.log(str);
+console.log(typeof str)
+
+let boolNum = Number(true);
+console.log(boolNum);
+console.log(typeof boolNum)
+
+let intVal = parseInt(123.45);
+console.log(intVal);
+console.log(typeof intVal)
+let floatVal = parseFloat("123.45");
+console.log(floatVal);
+console.log(typeof floatVal)
+
+// // Types of Control Statements in JavaScript
+// // Conditional Statements (Decision Making)
+// //if statement
+// //if..else statement
+// //if..else..if statement
+
+let a = 10
+if(a>10) {
+  console.log("a is greater")
+}
+else if(a<10) {
+  console.log("a is lesser")
+}
+else {
+  console.log("a is equal")
+}
+// //switch statement
+let day = 7;
+switch(day) {
+case 1: console.log("Monday"); break;
+case 2: console.log("Tuesday"); break;
+case 3: console.log("Wednesday"); break;
+default: console.log("Invalid day");
+ }
+// //Looping statements
+//     //while  loop
+let k = 1;
+while (k <= 5) {
+   console.log("Number:", k);
+  k++;
+}
+//     //do-while loop
+let i = 1;
+do {
+console.log("Value:", i);
+i++;
+} while (i <= 5);
+// for loop
+for (let j = 1; j <= 5; j++) {
+console.log("Count:", j);
+}
+// // for...in loop (iterate over object properties)
+let students = {name:"chandana",age:25}
+for(let key in students) {
+  console.log(key+":"+students[key])
+}
+// // for...of loop (iterate over arrays/iterables)
+let colors = ["red","green","blue"]
+for(let color of colors) {
+    console.log(color)
+}
+
+// Jumping Statements
+// // break → exits loop/switch immediately
+for (let i = 1; i <= 5; i++) {
+  if (i === 3) {
+    break;
+  }
+  console.log(i); // prints 1, 2
+ }
+
+// // continue → skips current iteration and moves to next
+for (let i = 1; i <= 5; i++) {
+     if (i === 3){
+      continue;
+     } 
+   console.log(i); // prints 1, 2, 4, 5
+}
---
+++ Basics of js/task1.js
@@ -0,0 +1,6 @@
+let name = "Chandana"
+    let age = 25
+    let qualification = "cse"
+    console.log(name)
+    console.log(age)
+    console.log(qualification)
