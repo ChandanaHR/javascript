8332fd58fa3b907401e89c7f9a9b54121a85e69b
Chandana HR <77909453+ChandanaHR@users.noreply.github.com>
2025-09-02 18:01:26 +0530

    Add files via upload

---
+++ Events in js/events1.js
@@ -0,0 +1,10 @@
+// In JavaScript, events are actions or occurrences that happen in the browser, which 
+// JavaScript can detect and respond to.
+// Ways to Handle Events
+//     Inline Event (HTML attribute)
+//     Using javascript property
+//      Using addEventListener()
+
+//Difference between normal events and addEventListener
+// .onclick → assigns a single function to the click event.
+// .addEventListener() → allows attaching multiple event handlers.
